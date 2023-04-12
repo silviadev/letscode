a1fdbd98dedf0507736201afab2a27a2b8e1a86d
Silvia Carvalho <silviacarvalhodev@gmail.com>
2023-04-12 20:52:23 -0300

    Números em ordem crescente

--- Javascript/Aula04/crescente.js
+++ Javascript/Aula04/crescente.js
@@ -3,20 +3,20 @@ let n2 = prompt("Digite o segundo número:");
 let n3 = prompt("Digite o terceiro número:");
 let aux = 0;
 
-if(n1 > n3){
-    aux = n3;
-    n3 = n1;
-    n1 = aux;
-}
-else if (n1 > n2){
-    aux = n2;
-    n2 = n1;
-    n1 = aux;
-}
-else (n2 > n3); {
-    aux = n3;
-    n3 = n2;
+if(n1 > n2){
+    aux = n1;
+    n1 = n2;
     n2 = aux;
+}
+if (n1 > n3){
+    aux = n1;
+    n1 = n3;
+    n3 = aux;
+}
+if (n2 > n3) {
+    aux = n2;
+    n2 = n3;
+    n3 = aux;
 }
 
 //console.log(n1, n2, n3)
